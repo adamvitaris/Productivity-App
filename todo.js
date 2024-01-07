fc75ea4814e8a0beaaf90d81b15c986650246c3a
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2024-01-07 21:06:43 +0100

    todo.js updated
    Working on the overline on finished tasks.

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const textInput = document.querySelector("input");
 const addBtn = document.querySelector("#add-btn");
 const listContainer = document.getElementById("list-container");
+let currentId = 0;
 
 addBtn.addEventListener("click", addTask);
 
@@ -9,13 +10,16 @@ function addTask() {
         alert("Please enter your new task!")
     }
     else{
+        currentId++;
+        const taskId = "task" + currentId;
         const newTaskContainer = document.createElement("div");
         const checkboxItem = document.createElement("div");
         const checkbox = document.createElement("input");
         const label = document.createElement("label");
 
+        checkboxItem.classList.add("checkbox-item");
         checkbox.setAttribute("type", "checkbox");
-        checkbox.id = "task" + (newTaskContainer.children.length + 1);
+        checkbox.setAttribute("id", taskId);
         label.setAttribute("for", checkbox.id);
         label.classList.add("checkbox-text");
         label.textContent = textInput.value;
@@ -26,5 +30,11 @@ function addTask() {
         listContainer.appendChild(newTaskContainer);
 
         textInput.value = "";
+
+        //Add overline to the finished tasks. Not finished yet...
+        checkboxItem.addEventListener("click", function taskDone() {
+            label.textContent.style.textDecoration = overline;
+        });
     }
 }
+
