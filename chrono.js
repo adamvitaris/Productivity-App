f7a29cc351eb464381b03ca6532b7386d5a299da
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2024-01-14 00:02:44 +0100

    Uptade js

--- chrono.js
+++ chrono.js
@@ -2,7 +2,12 @@ hourInp = document.getElementById("hours");
 minuteInp = document.getElementById("minutes");
 textInp = document.getElementById("text");
 btnStart = document.getElementById("start-btn");
-menuCont = document.getElementsByClassName("menu-container");
+
+hourDisplay = document.querySelector("#hourDisplay");
+minuteDisplay = document.querySelector("#minuteDisplay");
+textDisplay = document.querySelector(".current-task");
+
+menuCont = document.querySelector(".menu-container");
 menuCont.style.display = "none";
 
 btnStart.addEventListener("click", () => {
@@ -10,25 +15,12 @@ btnStart.addEventListener("click", () => {
     minuteValue = minuteInp.value;
     textValue = textInp.value;
 
-    /*menuCont = document.createElement("div");
-    counterCont = document.createElement("div");
-    hourFinal = document.createElement("h2");
-    hourFinal.innerHTML = hourValue;
-    hourFinal = document.createElement("h2");
-
-    btnCont = document.createElement("div");
-    btnStart = document.createElement("button");
-    btnPause = document.createElement("button");
-    btnStop = document.createElement("button");
-    btnReset = document.createElement("button");
-
-    menuCont.classList.add("menu-container");
-    counterCont.classList.add("counter-container");
-    btnCont.classList.add("btn-control");
-    btnStart.classList.add("btn");
-    btnPause.classList.add("btn");
-    btnStop.classList.add("btn");
-    btnReset.classList.add("btn");*/
+    // Put some zeros before the time setting.
+    // Make the countdown.
+    menuCont.style.display = "block";
+    hourDisplay.innerHTML = hourValue;
+    minuteDisplay.innerHTML = minuteValue;
+    textDisplay.innerHTML = textValue;
 });
 
 
