f133fdb26fc837b64cb7d54737c2415cec5edb03
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2024-01-14 11:44:17 +0100

    toggle menu update

--- chrono.js
+++ chrono.js
@@ -1,7 +1,7 @@
-hourInp = document.getElementById("hours");
-minuteInp = document.getElementById("minutes");
-textInp = document.getElementById("text");
-btnStart = document.getElementById("start-btn");
+hourInp = document.querySelector("#hours");
+minuteInp = document.querySelector("#minutes");
+textInp = document.querySelector("#text");
+btnStart = document.querySelector("#start-btn");
 
 hourDisplay = document.querySelector("#hourDisplay");
 minuteDisplay = document.querySelector("#minuteDisplay");
@@ -10,18 +10,27 @@ textDisplay = document.querySelector(".current-task");
 menuCont = document.querySelector(".menu-container");
 menuCont.style.display = "none";
 
-btnStart.addEventListener("click", () => {
+document.addEventListener("keydown", function (e){
+    if (e.key === "Enter") {
+        if (hourInp.value !== 0
+        && minuteInp.value !== 0
+        && textInp.value.length !== 0)
+         toggleMenu();
+        }
+    });
+
+btnStart.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
     hourValue = hourInp.value;
     minuteValue = minuteInp.value;
     textValue = textInp.value;
 
     // Put some zeros before the time setting.
     // Make the countdown.
+    // BUG - Limit the characters of the Task input!
     menuCont.style.display = "block";
     hourDisplay.innerHTML = hourValue;
     minuteDisplay.innerHTML = minuteValue;
     textDisplay.innerHTML = textValue;
-});
-
-
-
+}
