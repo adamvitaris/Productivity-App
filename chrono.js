6e46bff4b3eb98c8f6485280222fbbe2aa31b608
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2024-01-30 11:07:04 +0100

    update

--- chrono.js
+++ chrono.js
@@ -11,11 +11,15 @@ let textDisplay = document.querySelector(".current-task");
 let menuCont = document.querySelector(".menu-container");
 menuCont.style.display = "none";
 
+let totalSeconds = 0;
+
 document.addEventListener("keydown", function (e){
     let hourInp = parseInt(hour.value);
     let minuteInp = parseInt(minute.value);
     let textInp = text.value;
     if (e.key === "Enter") {
+            totalSeconds = (hourInp * 3600) + (minuteInp * 60);
+            console.log(totalSeconds);
             toggleMenu(hourInp, minuteInp, textInp);
             start(totalSeconds);
         }
@@ -28,9 +32,9 @@ btnEnter.addEventListener("click", function() {
     let minuteInp = parseInt(minute.value);
     let textInp = text.value;
     if (hourInp != 0 && minuteInp != 0) {
-        let totalSeconds = (hourInp * 3600) + (minuteInp * 60);
+        totalSeconds = (hourInp * 3600) + (minuteInp * 60);
         toggleMenu(hourInp, minuteInp, textInp);
-        start(totalSeconds);
+        start();
     }
 });
 
@@ -48,20 +52,28 @@ function toggleMenu(hour, minute, text) {
 //----------------------------------------------
 let interval = null;
 
-function updateInputs(totalSeconds) {
-    const minuteUpdate = Math.floor((totalSeconds % 3600) / 60);
-    const hourUpdate = Math.floor(totalSeconds / 3600);
+function updateInputs() {
+    console.log(totalSeconds);
+    let hourUpdate = Math.floor(totalSeconds / 3600);
+    let minuteUpdate = Math.floor((totalSeconds % 3600) / 60);
+    totalSeconds--;
 
-    minute.value = minuteUpdate;
-    hour.value = hourUpdate;
+    hourUpdate = hour.toString().padStart(2, '0');
+    minuteUpdate = minute.toString().padStart(2, '0');
+
+    hourDisplay.innerHTML = hourUpdate;
+    minuteDisplay.innerHTML = minuteUpdate;
+
+    console.log(minuteUpdate);
+    console.log(hourUpdate);
+
 }
 
-function start(totalSeconds) {
+function start() {
     let hour = document.querySelector("#hours");
     let minute = document.querySelector("#minutes");
     interval = setInterval(() => {
-        totalSeconds--;
-        updateInputs(totalSeconds);
+        updateInputs();
 
         if (totalSeconds <= 0) {
             interval = clearInterval(interval);
---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+
+let num = 0;
+function countUp() {
+let myInterval = setInterval(()=> {
+    num ++;
+    console.log(num);
+    if (num >= 4) {
+        console.log("X");
+        clearInterval(myInterval);
+    }
+}, 1000);
+}
+
+countUp();
